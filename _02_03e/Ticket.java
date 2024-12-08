9280ee30291b7cfa35e15e3d5ad5dca85cd0ce35
Alejandro Suárez González <72723432+ASuarez18@users.noreply.github.com>
2024-12-08 06:21:32 +0000

    Course Finished

--- _02_03e/Ticket.java
+++ _02_03e/Ticket.java
@@ -10,4 +10,30 @@ public class Ticket {
   private double price;
   private boolean isReturn;
 
+  // Add three methods to set the value of each field, called setDestination,
+  // setPrice and setIsReturn.
+  public void setDestination(String _destination){
+    this.destination = _destination;
+  }
+
+  public void setPrice(double _price){
+    this.price = _price;
+  }
+
+  public void setIsReturn(boolean _isReturn){
+    this.isReturn = _isReturn;
+  }
+  // Add a separate method to get the value of each field, called getDestination,
+  // getPrice and getIsReturn.
+  public String getDestination(){
+    return destination;
+  }
+
+  public double getPrice(){
+    return price;
+  }
+
+  public boolean getIsReturn(){
+    return isReturn;
+  }
 }
